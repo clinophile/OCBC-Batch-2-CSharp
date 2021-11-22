f3f1e0edac68e00a7d96128d2ab2908c0c444534
Dini Puspita <clinophile13@gmail.com>
2021-11-22 13:49:08 +0700

    Belajar c#

---
+++ Sesi01/HelloMono/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloMono
+{
+    class Hello
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
