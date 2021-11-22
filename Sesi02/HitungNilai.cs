400ab11e665719c3e98a5021f671e67b6d5441f9
Dini Puspita <clinophile13@gmail.com>
2021-11-22 15:07:19 +0700

    adding Sesi 2

---
+++ Sesi02/HitungNilai.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+    public class HitungNilai
+    {
+        static void Main(string[] args)
+        {
+            int pertama, kedua, ketiga, total;
+            double rata;
+
+            Console.WriteLine("=== Menghitung Nilai ===");
+            Console.Write("Masukan nilai PERTAMA: ");
+            pertama = int.Parse(Console.ReadLine());
+            Console.Write("Masukan nilai KEDUA: ");
+            kedua = int.Parse(Console.ReadLine());
+            Console.Write("Masukan nilai KETIGA: ");
+            ketiga = int.Parse(Console.ReadLine());
+
+            total = pertama + kedua + ketiga ;
+            rata = total / 3.0 ;
+            Console.WriteLine("Total Nilai adalah: " + total);
+            Console.WriteLine("Rata-rata Nilai adalah: " + rata);
+            Console.Read();
+        }
+    }
+
---
+++ Sesi02/helloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+    public class helloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nama saya Dini!");
+            Console.WriteLine("Selamat Datang di Program BootCamp OCBC");
+            Console.WriteLine("Tahap 1 saya akan belajar C#");
+        }
+    }
+
