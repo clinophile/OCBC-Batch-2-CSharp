4c046d7c8ff4b06cfb8acb7907fab9414ffbd715
Dini Puspita <clinophile13@gmail.com>
2021-11-24 11:03:29 +0700

    modifying program5

--- Sesi05/Program5/Program.cs
+++ Sesi05/Program5/Program.cs
@@ -24,10 +24,10 @@ namespace program5
 
         public override void penjumlahanperkaliandll() {
             Console.WriteLine("Overiding 2.....----->>>>>");
-            Console.WriteLine("Hasil penjumlahan angka {0} dan angka {1} = {2}",
-            bil1, bil2, bil1+bil2);
-            Console.WriteLine("Hasil perkalian angka {0} dan angka {1} = {2}",
-            bil1, bil2, bil1*bil2);
+            Console.WriteLine("Hasil pembagian angka {0} dan angka {1} = {2}",
+            bil1, bil2, bil1/bil2);
+            Console.WriteLine("Hasil pengurangan angka {0} dan angka {1} = {2}",
+            bil1, bil2, bil1-bil2);
         }
 
 
